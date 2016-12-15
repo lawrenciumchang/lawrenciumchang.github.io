61305e8e115cd9b784be5078ad40df75a307b352
Lawrence Chang <lchang@credera.com>
2016-12-14 20:33:49 -0600

    added photo mask to images on photos page

--- js/imgPreload/imgPreload.directive.js
+++ js/imgPreload/imgPreload.directive.js
@@ -2,14 +2,14 @@
 
 angular.directive('imgPreload', ['$rootScope', function($rootScope) {
     return {
-      restrict: 'A',
-      scope: {
-        ngSrc: '@'
-      },
-      link: function(scope, element, attrs) {
-        element.on('load', function() {
-          element.addClass('reveal-image');
-        });
-      }
+        restrict: 'A',
+        scope: {
+            ngSrc: '@'
+        },
+        link: function(scope, element, attrs) {
+            element.on('load', function() {
+                element.addClass('reveal-image');
+            });
+        }   
     };
 }]);
