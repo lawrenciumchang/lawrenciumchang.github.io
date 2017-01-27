4025fd05822ffee2c710256b00f814d1a86301d3
Lawrence Chang <lchang@credera.com>
2017-01-27 10:51:53 -0600

    added accordion to menu bar on mobile

--- js/sidebar/sidebar.controller.js
+++ js/sidebar/sidebar.controller.js
@@ -22,10 +22,27 @@ function SidebarController($q) {
         vm.showMenuItems = !vm.showMenuItems;
     }
 
-    $(document).ready(function(){
-        $('#nav-icon').click(function(){
-            $(this).toggleClass('open');
-        });
+    // $(document).ready(function(){
+    //     $('#nav-icon').click(function(){
+    //         $(this).toggleClass('open');
+    //     });
+    // });
+
+    $(function() {
+      $('.acc_ctrl').on('click', function(e) {
+        e.preventDefault();
+        if ($(this).hasClass('active')) {
+          $(this).removeClass('active');
+          $(this).next()
+          .stop()
+          .slideUp(300);
+        } else {
+          $(this).addClass('active');
+          $(this).next()
+          .stop()
+          .slideDown(300);
+        }
+      });
     });
 
 }
