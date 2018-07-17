6708aeb608ab1094200c79bf74d979de36d602a1
Lawrence Chang <lawrencium.chang@gmail.com>
2018-07-16 22:03:42 -0500

    added typing interaction

--- js/landing/landing.controller.js
+++ js/landing/landing.controller.js
@@ -12,8 +12,52 @@ function LandingController($q) {
     function activate() {
         var promises = [];
         return $q.all(promises).then(function() {
-
+            displayFirstSentence();
         });
     }
 
+    function displayFirstSentence() {
+        var typed = new Typed('.landing-text .first', {
+            strings: ["<span class='font-regular blue'>Hi, I'm Lawrence.</span>"],
+            typeSpeed: 40,
+            showCursor: false,
+            onComplete: function() {
+                displaySecondSentence();
+            }
+        });
+    };
+
+    function displaySecondSentence() {
+        var typed = new Typed('.landing-text .second', {
+            strings: ["<span class='font-light dark-gray'> A technology consultant and software developer located in Dallas, TX.</span>"],
+            typeSpeed: 40,
+            showCursor: false,
+            startDelay: 500,
+            onComplete: function() {
+                displayThirdSentence();
+            }
+        });
+    };
+
+    function displayThirdSentence() {
+        var typed = new Typed('.landing-text .third', {
+            strings: ["<span class='font-light dark-gray'> I like making things.</span>"],
+            typeSpeed: 40,
+            showCursor: false,
+            startDelay: 500,
+            onComplete: function() {
+                displayFourthSentence();
+            }
+        });
+    };
+
+    function displayFourthSentence() {
+        var typed = new Typed('.landing-text .fourth', {
+            strings: ["<span class='font-light dark-gray'> I also take photos.</span>"],
+            typeSpeed: 40,
+            showCursor: false,
+            startDelay: 500
+        });
+    };
+    
 }
