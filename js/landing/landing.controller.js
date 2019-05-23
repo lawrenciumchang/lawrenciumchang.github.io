ddaa309530f55aa75144fd1f6da34718e6aaec80
Lawrence Chang <lawrencium.chang@gmail.com>
2019-05-23 15:11:08 -0500

    added new up arrow hover icon

--- js/landing/landing.controller.js
+++ js/landing/landing.controller.js
@@ -6,16 +6,16 @@ app
 /* @ngInject */
 function LandingController($q) {
     var vm = this;
-    // vm.scrollToTop = scrollToTop;
+    vm.scrollToTop = scrollToTop;
     // vm.navigateToPhotos = navigateToPhotos;
-    // vm.gaTrackClick = gaTrackClick;
+    vm.gaTrackClick = gaTrackClick;
 
-    // function gaTrackClick(category, label) {
-    //     var host = window.location.hostname;
-    //     if (host != 'localhost') {
-    //         ga('send', 'event', category, 'click' , label);
-    //     }
-    // };
+    function gaTrackClick(category, label) {
+        var host = window.location.hostname;
+        if (host != 'localhost') {
+            ga('send', 'event', category, 'click' , label);
+        }
+    };
 
     activate();
 
@@ -64,7 +64,7 @@ function LandingController($q) {
         });
     };
 
-    // function scrollFunction() {
+    function scrollFunction() {
     //     // Hides down navigation arrow on scroll, reveals when scrolled back to top.
     //     if (document.body.scrollTop > (window.innerHeight/2) || document.documentElement.scrollTop > (window.innerHeight/2)) {
     //         var navigationArrow = document.querySelector('.navigation-arrow');
@@ -75,22 +75,22 @@ function LandingController($q) {
     //         navigationArrow.classList.add('reveal');
     //     }
 
-    //     // Controls scroll reveal for hover button elements. Use .hide-hover-btn class on element to reveal/hide past first panel. 
-    //     if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
-    //         var elements = document.querySelectorAll('.hide-hover-btn');
-    //         angular.forEach(elements, function(el) {
-    //             el.classList.add('show-hover-btn');
-    //         }); 
-    //     } else {
-    //         var elements = document.querySelectorAll('.show-hover-btn');
-    //         angular.forEach(elements, function(el) {
-    //             el.classList.remove('show-hover-btn');
-    //         }); 
-    //     }
-    // }
+        // Controls scroll reveal for hover button elements. Use .hide-hover-btn class on element to reveal/hide past first panel. 
+        if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
+            var elements = document.querySelectorAll('.hide-hover-btn');
+            angular.forEach(elements, function(el) {
+                el.classList.add('show-hover-btn');
+            }); 
+        } else {
+            var elements = document.querySelectorAll('.show-hover-btn');
+            angular.forEach(elements, function(el) {
+                el.classList.remove('show-hover-btn');
+            }); 
+        }
+    }
 
-    // function scrollToTop() {
-    //     Jump('.header-template');
-    // }
+    function scrollToTop() {
+        Jump('.header-template');
+    }
 
 }
