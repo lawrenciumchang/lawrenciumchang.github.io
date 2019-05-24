15476055d86563a697284f99cc338bda24c88476
Lawrence Chang <lawrencium.chang@gmail.com>
2019-05-23 23:33:35 -0500

    updated mobile menu

--- js/header/header.controller.js
+++ js/header/header.controller.js
@@ -6,6 +6,7 @@ app
 /* @ngInject */
 function HeaderController($q) {
     var vm = this;
+    vm.openMenu = openMenu;
     vm.gaTrackClick = gaTrackClick;
 
     function gaTrackClick(category, label) {
@@ -24,27 +25,49 @@ function HeaderController($q) {
         });
     }
 
-    $('.menu-burger, .menu-items').on('click', function() {
-        $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
-        $('.menu-burger').text() == "☰" ? openMenu() : closeMenu();
-    });
+    // $('.menu-burger, .menu-items').on('click', function() {
+    //     $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+    //     $('.menu-burger').text() == "☰" ? openMenu() : closeMenu();
+    // });
 
-    function openMenu() {
-        $('.menu-burger').text('✕');
-        // Lock scroll
-        $('body').css({'overflow':'0'});
-        $(document).bind('scroll',function () { 
-            window.scrollTo(0,0); 
-        });
-        gaTrackClick('mobile menu', 'open');
-    };
+    function openMenu(element) {
+        element.classList.toggle('active');
 
-    function closeMenu() {
-        $('.menu-burger').text('☰');
-        // Enable scroll
-        $(document).unbind('scroll'); 
-        $('body').css({'overflow':'visible'});
-        gaTrackClick('mobile menu', 'close');
-    };
+        if (element.classList.contains('active')) {
+            document.querySelector('.menu-overlay').classList.add('active');
+            document.querySelector('.menu-contents').classList.add('active');
+            // Lock scroll
+            $('body').css({'overflow':'0'});
+            $(document).bind('scroll',function () { 
+                window.scrollTo(0,0); 
+            });
+            gaTrackClick('mobile menu', 'open');
+        } else {
+            document.querySelector('.menu-overlay').classList.remove('active');
+            document.querySelector('.menu-contents').classList.remove('active');
+            // Enable scroll
+            $(document).unbind('scroll'); 
+            $('body').css({'overflow':'visible'});
+            gaTrackClick('mobile menu', 'close');
+        }
+    }
+
+    // function openMenu() {
+    //     $('.menu-burger').text('✕');
+    //     // Lock scroll
+    //     $('body').css({'overflow':'0'});
+    //     $(document).bind('scroll',function () { 
+    //         window.scrollTo(0,0); 
+    //     });
+    //     gaTrackClick('mobile menu', 'open');
+    // };
+
+    // function closeMenu() {
+    //     $('.menu-burger').text('☰');
+    //     // Enable scroll
+    //     $(document).unbind('scroll'); 
+    //     $('body').css({'overflow':'visible'});
+    //     gaTrackClick('mobile menu', 'close');
+    // };
 
 }
