d3b9a2ec1170ed10f1d772baed29aa5555ba20e5
Lawrence Chang <lawrencium.chang@gmail.com>
2020-07-26 14:53:34 -0500

    post template setup

--- src/components/PostTemplate.vue
+++ src/components/PostTemplate.vue
@@ -1,19 +1,48 @@
 <template>
-  <p>Post Template {{ postId }}</p>
+  <div class="post-template">
+    <div class="title-container section">
+      <h1>{{ post.title }}</h1>
+      <p class="description">{{ post.shortDescription }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import posts from '@/data/posts.json';
+
 export default {
   name: 'PostTemplate',
   props: {
     postId: String
   },
-  mounted: function() {
-    console.log('props: ', this.postId);
+  data() {
+    return {
+      post: this.getPost(this.postId)
+    }
+  },
+  methods: {
+    getPost: function(postId) {
+      let post = [];
+      posts.forEach(element => {
+        if (postId == element.id) {
+          post = element;
+        }
+      });
+      return post;
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+.post-template {
+  .section {
+    padding: 80px 0;
+  }
 
+  .title-container {
+    border-bottom: 1px solid $gray-border;
+    margin-top: 100px;
+  }
+}
 </style>
