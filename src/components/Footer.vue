0fc5fe913879e440c44fc3b298f6e602be1919f1
Lawrence Chang <github.cables@aleeas.com>
2021-07-19 18:44:02 -0500

    updated footer content and styles

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="footer">
     <div class="container">
-      <h2 v-scroll-reveal class="hello">Hello there!</h2>
-      <h2 v-scroll-reveal class="message">Want to create something together or just chat? Let’s grab a virtual coffee and talk about anything from photography to design to mechanical keyboards.</h2>
-      <a v-scroll-reveal data-clipboard-text="contact@lawrencechang.design" @click="revealCopiedText(); gaTrackClick('Footer', 'Get In Touch CTA')">Get in touch</a>
-      <span ref="copiedText" class="copied-text">My email has been copied to your clipboard!</span>
+      <div class="top">
+        <div class="left">
+          <p v-scroll-reveal class="regular hello">Thanks for stopping by!</p>
+          <p v-scroll-reveal class="regular message">Want to create something together or just chat? Let’s grab a virtual coffee and talk about anything from photography to design to mechanical keyboards.</p>
+          <div v-scroll-reveal class="get-in-touch-container">
+            <a class="get-in-touch large" data-clipboard-text="contact@lawrencechang.design" @click="revealCopiedText(); gaTrackClick('Footer, Get In Touch')">get in touch</a>
+            <span ref="copiedText" class="copied-text">My email has been copied to your clipboard!</span>
+          </div>
+        </div>
+        <div class="right">
+          <div class="group">
+            <router-link to="" @click="gaTrackClick('Footer, About')">about</router-link>
+            <router-link to="" @click="gaTrackClick('Footer, Resume')">resume</router-link>
+          </div>
+          <div class="group">
+            <router-link to="" @click="gaTrackClick('Footer, Clackd')">clackd</router-link>
+            <router-link to="" @click="gaTrackClick('Footer, Credera')">credera</router-link>
+            <router-link to="" @click="gaTrackClick('Footer, WFF')">wff</router-link>
+          </div>
+          <div class="group">
+            <router-link to="" @click="gaTrackClick('Footer, Photography')">photography</router-link>
+          </div>
+          <div class="group">
+            <router-link to="" @click="gaTrackClick('Footer, LinkedIn')">linkedin</router-link>
+            <router-link to="" @click="gaTrackClick('Footer, flickr')">flickr</router-link>
+          </div>
+        </div>
+      </div>
       <div class="sub-section">
         <span class="copyright">Copyright © {{ currentYear }} Lawrence Chang</span>
       </div>
@@ -44,16 +68,46 @@ export default {
 
 <style scoped lang="scss">
 .footer {
-  background-color: $gray-01;
+  @include theme() {
+    background-color: theme-get('footer-bg-color');
+  }
 
   .container {
     @include page-layout;
-    padding: 120px 24px 100px;
+    padding: 100px 24px;
 
-    .hello, .message {
-      display: block;
-      font-size: 36px;
-      letter-spacing: 1px;
+    .top {
+      display: grid;
+      grid-template-columns: 2fr 1fr;
+      margin-bottom: 100px;
+
+      @media (max-width: $mobile-breakpoint) {
+        grid-template-columns: 1fr;
+      }
+
+      .left {
+        margin-bottom: 20px;
+      }
+
+      .right {
+        .group {
+          &:not(:last-child) {
+            margin-bottom: 40px;
+          }
+          a {
+            display: block;
+            color: $white;
+            text-align: right;
+            &:not(:last-child) {
+              margin-bottom: 12px;
+            }
+
+            @media (max-width: $mobile-breakpoint) {
+              text-align: left;
+            }
+          }
+        }
+      }
     }
 
     .hello {
@@ -62,18 +116,20 @@ export default {
     }
 
     .message {
-      color: $gray-06;
-      line-height: 48px;
-      margin-bottom: 40px;
+      color: $gray-05;
+      margin-bottom: 32px;
     }
 
-    .btn-primary {
-      margin-bottom: 16px;
+    .get-in-touch {
+      display: block;
+      color: $blue-primary;
+      cursor: pointer;
+      margin-bottom: 12px;
     }
 
     .copied-text{
-      color: $blue-primary;
-      font-size: 14px;
+      color: $white;
+      font-size: 12px;
       opacity: 0;
       transition: $ease-in-out;
 
@@ -82,30 +138,10 @@ export default {
       }
     }
 
-    .sub-section {
-      margin-top: 140px;
-
-      .copyright, .built {
-        color: $white;
-        display: block;
-        font-size: 16px;
-        font-weight: 500;
-        line-height: 24px;
-        margin-bottom: 4px;
-      }
-
-      a {
-        text-decoration: none;
-      }
-
-      .github-logo {
-        background: url('/assets/icons/github-logo.svg') no-repeat;
-        display: inline-block;
-        height: 18px;
-        position: relative;
-        top: 5px;
-        width: 18px;
-      }
+    .copyright {
+      color: $gray-05;
+      display: block;
+      font-size: 14px;
     }
   }
 }
