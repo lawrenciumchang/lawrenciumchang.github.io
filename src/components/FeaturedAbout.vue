9aaf59b3a52ab7583397245a618a96f74a9eb08a
Lawrence Chang <github.cables@aleeas.com>
2021-07-20 08:30:52 -0500

    added featured about section

--- src/components/FeaturedAbout.vue
+++ src/components/FeaturedAbout.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="featured-about">
-
+    <h1 v-scroll-reveal>about</h1>
+    <p v-scroll-reveal class="regular first-sentence">I am a UX Designer based in Houston, TX.</p>
+    <p v-scroll-reveal class="regular second-sentence">I lead design at companies of various stages, turning startup ideas into tangible products and helping clients transform their digital needs into world-class experiences.</p>
+    <router-link v-scroll-reveal to="/about" @click="gaTrackClick('Home', 'Read More')">read more</router-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FeaturedAbout'
+  name: 'FeaturedAbout',
+  methods: {
+    gaTrackClick: function(category, label) {
+      this.$ga.event(category, 'click', label);
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .featured-about {
+  @include page-layout;
   padding-bottom: 100px;
+
+  .first-sentence {
+    margin-bottom: 12px;
+    @include theme() {
+      color: theme-get('default-theme-inverse');
+    }
+  }
+
+  .second-sentence {
+    margin-bottom: 20px;
+  }
 }
 </style>
